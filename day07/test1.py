3ec319686c32958a412a885c0ff55cc1e1ef8a78
abarhub <tulipebleu1@laposte.net>
2025-05-04 13:32:52 +0200

    reformatage du code

--- day07/test1.py
+++ day07/test1.py
@@ -3,6 +3,7 @@ from itertools import product
 
 print('hello world')
 
+
 @dataclass
 class Data:
     valeur: int
@@ -19,56 +20,56 @@ class Resultat:
     operateurs: list[str]
 
 
-def lecture(fichier)-> list[Data]:
-    res=[]
+def lecture(fichier) -> list[Data]:
+    res = []
     with open(fichier, 'r') as file:
         # Read each line in the file
         for line in file:
             # Print each line
-            s=line.strip()
-            t=s.split(':')
-            t2=t[1].split()
-            tab=[]
+            s = line.strip()
+            t = s.split(':')
+            t2 = t[1].split()
+            tab = []
             for x in t2:
                 tab.append(int(x))
-            d=Data(int(t[0]),tab)
-            #print(line.strip())
-            #print(d)
+            d = Data(int(t[0]), tab)
+            # print(line.strip())
+            # print(d)
             res.append(d)
 
     return res
 
-def cherche_operateurs(data:Data)->Resultat|None:
 
-    liste_operateurs=['+','*']
+def cherche_operateurs(data: Data) -> Resultat | None:
+    liste_operateurs = ['+', '*']
 
-    longueur = len(data.nombres)-1
+    longueur = len(data.nombres) - 1
     combinaisons = [''.join(p) for p in product(liste_operateurs, repeat=longueur)]
 
     for combinaison in combinaisons:
         valeur = 0
         for i in range(len(data.nombres)):
-            if i==0:
-                valeur=data.nombres[i]
+            if i == 0:
+                valeur = data.nombres[i]
             else:
-                op2=combinaison[i-1]
-                v=data.nombres[i]
-                if op2=='+':
-                    valeur=valeur+v
-                elif op2=='*':
-                    valeur=valeur*v
-        if valeur==data.valeur:
-            return Resultat(valeur,data.nombres,combinaison.split())
+                op2 = combinaison[i - 1]
+                v = data.nombres[i]
+                if op2 == '+':
+                    valeur = valeur + v
+                elif op2 == '*':
+                    valeur = valeur * v
+        if valeur == data.valeur:
+            return Resultat(valeur, data.nombres, combinaison.split())
 
     return None
 
 
-liste1=lecture('test1.txt')
+liste1 = lecture('test1.txt')
 print(liste1)
 
 for x in liste1:
-    op=cherche_operateurs(x)
+    op = cherche_operateurs(x)
     if op is not None:
-        print('trouve:',x,op)
+        print('trouve:', x, op)
     else:
-        print('non trouve:',x)
+        print('non trouve:', x)
