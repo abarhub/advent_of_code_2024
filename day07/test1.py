426ff149641772657f75963b0dcbea82d64d90db
abarhub <abarhub@gmail.com>
2025-05-11 17:05:17 +0200

    parcourt de l'arbre en arrétant si le nombre est trop grand

--- day07/test1.py
+++ day07/test1.py
@@ -15,6 +15,11 @@ class Resultat:
     nombres: list[int]
     operateurs: list[str]
 
+@dataclass
+class NbOperations:
+    nb: int
+    nbFeuilles: int
+    abandon: int
 
 def lecture(fichier) -> list[Data]:
     res = []
@@ -36,7 +41,7 @@ def lecture(fichier) -> list[Data]:
     return res
 
 
-def cherche_operateurs(data: Data) -> Resultat | None:
+def cherche_operateurs(data: Data, nb_op: NbOperations) -> Resultat | None:
     liste_operateurs = ['+', '*']
 
     longueur = len(data.nombres) - 1
@@ -52,14 +57,18 @@ def cherche_operateurs(data: Data) -> Resultat | None:
                 v = data.nombres[i]
                 if op2 == '+':
                     valeur = valeur + v
+                    nb_op.nb+=1
                 elif op2 == '*':
                     valeur = valeur * v
+                    nb_op.nb+=1
         if valeur == data.valeur:
+            nb_op.nbFeuilles+=1
             return Resultat(valeur, data.nombres, combinaison.split())
 
     return None
 
-def parcourt(data: Data, liste_operateurs: list[str],no:int,val:int, operateurs_selectionnee:list[str]) -> Resultat | None:
+def parcourt(data: Data, liste_operateurs: list[str],no:int,val:int,
+             operateurs_selectionnee:list[str], nb_op: NbOperations) -> Resultat | None:
 
     if no<0:
         raise IndexError("no="+str(no))
@@ -68,38 +77,80 @@ def parcourt(data: Data, liste_operateurs: list[str],no:int,val:int, operateurs_
     res=-1
     if no == 0:
         v=data.nombres[no]
-        return parcourt(data, liste_operateurs, no+1, v,[])
+        return parcourt(data, liste_operateurs, no+1, v,[], nb_op)
     else:
         for operateur in liste_operateurs:
             if operateur == '+':
                 v2=val+data.nombres[no]
+                nb_op.nb+=1
             elif operateur == '*':
                 v2=val*data.nombres[no]
+                nb_op.nb+=1
             else:
                 raise Exception("Erreur")
             if no>=len(data.nombres)-1:
                 if v2 == data.valeur:
                     liste2=operateurs_selectionnee.copy()
                     liste2.append(operateur)
+                    nb_op.nbFeuilles+=1
                     return Resultat(data.valeur, data.nombres, liste2)
             else:
                 liste2=operateurs_selectionnee.copy()
                 liste2.append(operateur)
-                res2=parcourt(data, liste_operateurs, no+1, v2, liste2)
+                res2=parcourt(data, liste_operateurs, no+1, v2, liste2, nb_op)
                 if res2 is not None:
                     return res2
     return None
 
-def cherche_operateurs2(data: Data) -> Resultat | None:
+
+def parcourt2(data: Data, liste_operateurs: list[str],no:int,val:int,
+              operateurs_selectionnee:list[str], nb_op: NbOperations) -> Resultat | None:
+
+    if no<0:
+        raise IndexError("no="+str(no))
+    elif no>=len(data.nombres):
+        raise IndexError("no="+str(no)+",len(data.nombres)="+str(len(data.nombres)))
+    res=-1
+    if no == 0:
+        v=data.nombres[no]
+        return parcourt2(data, liste_operateurs, no+1, v,[],nb_op)
+    else:
+        for operateur in liste_operateurs:
+            if operateur == '+':
+                v2=val+data.nombres[no]
+                nb_op.nb+=1
+            elif operateur == '*':
+                v2=val*data.nombres[no]
+                nb_op.nb+=1
+            else:
+                raise Exception("Erreur")
+            if v2>data.valeur:
+                nb_op.abandon+=1
+                continue
+            if no>=len(data.nombres)-1:
+                if v2 == data.valeur:
+                    liste2=operateurs_selectionnee.copy()
+                    liste2.append(operateur)
+                    nb_op.nbFeuilles+=1
+                    return Resultat(data.valeur, data.nombres, liste2)
+            else:
+                liste2=operateurs_selectionnee.copy()
+                liste2.append(operateur)
+                res2=parcourt2(data, liste_operateurs, no+1, v2, liste2, nb_op)
+                if res2 is not None:
+                    return res2
+    return None
+
+def cherche_operateurs2(data: Data, nb_op: NbOperations) -> Resultat | None:
     liste_operateurs = ['+', '*']
 
-    return parcourt(data,liste_operateurs,0,0,[])
+    return parcourt2(data, liste_operateurs, 0, 0, [], nb_op)
 
-def cherche_operateurs_choix(data: Data, no: int) -> Resultat | None:
+def cherche_operateurs_choix(data: Data, no: int, nb_op: NbOperations) -> Resultat | None:
     if no == 0:
-        return cherche_operateurs(data)
+        return cherche_operateurs(data,nb_op)
     elif no == 1:
-        return cherche_operateurs2(data)
+        return cherche_operateurs2(data,nb_op)
     else:
         raise Exception("Erreur")
 
@@ -111,8 +162,9 @@ def recherche(no:int):
 
     total=0
     nb=0
+    nbOp=NbOperations(0,0,0)
     for x in liste1:
-        op = cherche_operateurs_choix(x, no)
+        op = cherche_operateurs_choix(x, no, nbOp)
         if op is not None:
             print('trouve:', x, op)
             nb+=1
@@ -121,10 +173,11 @@ def recherche(no:int):
             print('non trouve:', x)
     print('nb:', nb)
     print('total:', total)
+    print('nb op:', nbOp)
 
 if True:
     no=0
-    #no=1
+    no=1
 
     recherche(no)
 else:
